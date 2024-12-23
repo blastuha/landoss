58f5a89e80227e1904a69de304fa34097a2a990f
Boris <shevnin.boris2@yandex.ru>
2024-12-23 18:19:16 +0300

    Benefits добавил логику

--- src/components/sections/Benefits/Benefits.tsx
+++ src/components/sections/Benefits/Benefits.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import Image from "next/image";
 import styles from "./Benefits.module.scss";
 
 export const Benefits = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const items = [
+    "Полный контроль над ИИ-инструментами",
+    "Индивидуальное обучение и практические результаты",
+    "Решение конкретных задач вашего бизнеса",
+    "Защиту и безопасность ваших данных",
+    "Подготовку вашей команды к работе с ИИ",
+  ];
+
+  const getImageSrc = (index: number) =>
+    `/benefitsSection/benefitsImg${index + 1}.png`;
+
   return (
     <section className={styles["benefits"]}>
       <h2 className={styles["benefits__title"]}>Что вы получите?</h2>
@@ -12,30 +26,24 @@ export const Benefits = () => {
       <div className={styles["benefits__content"]}>
         {/* Левая часть со списком блоков */}
         <div className={styles["benefits__left"]}>
-          <div
-            className={`${styles["benefits__item"]} ${styles["benefits__item--active"]}`}
-          >
-            Полный контроль над ИИ-инструментами
-          </div>
-          <div className={styles["benefits__item"]}>
-            Индивидуальное обучение <br />и практические результаты
-          </div>
-          <div className={styles["benefits__item"]}>
-            Решение конкретных задач вашего бизнеса
-          </div>
-          <div className={styles["benefits__item"]}>
-            Защиту и безопасность ваших данных
-          </div>
-          <div className={styles["benefits__item"]}>
-            Подготовку вашей команды к работе <br />с ИИ
-          </div>
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={classNames(styles["benefits__item"], {
+                [styles["benefits__item--active"]]: activeIndex === index,
+              })}
+              onClick={() => setActiveIndex(index)}
+            >
+              {item}
+            </div>
+          ))}
         </div>
 
         {/* Правая часть с изображением */}
         <div className={styles["benefits__right"]}>
           <Image
-            src="/benefitsImg.png"
-            alt="Иллюстрация руки-робота"
+            src={getImageSrc(activeIndex)}
+            alt={`Иллюстрация для блока ${items[activeIndex]}`}
             width={560}
             height={560}
             priority
