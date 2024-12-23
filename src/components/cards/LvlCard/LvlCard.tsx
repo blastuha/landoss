c4b23112db768b9dc7e6abe76c6db72b3ea277bd
Boris <shevnin.boris2@yandex.ru>
2024-12-23 14:23:37 +0300

    Сделал секцию ChooseLvl без адаптива

--- src/components/cards/LvlCard/LvlCard.tsx
+++ src/components/cards/LvlCard/LvlCard.tsx
@@ -3,13 +3,20 @@
 import React from "react";
 import styles from "./LvlCard.module.scss";
 
+interface GroupedBulletPoints {
+  groupTitle: string;
+  items: string[];
+}
+
 interface LvlCardProps {
   heading: string;
   price: string;
   snippet: string;
-  bulletPoints: string[];
+  bulletPoints: GroupedBulletPoints[];
   buttonText: string;
   credit?: string;
+  snippet2?: string;
+  snippet2Sticker?: string;
 }
 
 export const LvlCard: React.FC<LvlCardProps> = ({
@@ -19,27 +26,40 @@ export const LvlCard: React.FC<LvlCardProps> = ({
   bulletPoints,
   buttonText,
   credit,
+  snippet2,
+  snippet2Sticker,
 }) => {
   return (
     <div className={styles["lvl-card"]}>
-      {/* Верхняя часть карточки (цветной блок) */}
       <div className={styles["lvl-card__header"]}>
         <h3 className={styles["lvl-card__level"]}>{heading}</h3>
         <p className={styles["lvl-card__price"]}>{price}</p>
         {credit && <p className={styles["lvl-card__credit"]}>{credit}</p>}
       </div>
-
-      {/* Основной контент (темный блок) */}
       <div className={styles["lvl-card__body"]}>
         <p className={styles["lvl-card__subtitle"]}>{snippet}</p>
-        <ul className={styles["lvl-card__list"]}>
-          {bulletPoints.map((point, i) => (
-            <li key={i}>{point}</li>
-          ))}
-        </ul>
-      </div>
+        <p className={styles["lvl-card__subtitle2"]}>
+          {snippet2Sticker && (
+            <span className={styles["lvl-card__sticker"]}>
+              {snippet2Sticker}
+            </span>
+          )}
+          <span className={styles["lvl-card__subtitle2-text"]}>{snippet2}</span>
+        </p>
 
-      {/* Кнопка внизу */}
+        {bulletPoints.map((group, index) => (
+          <div key={index} className={styles["lvl-card__group"]}>
+            <p className={styles["lvl-card__group-title"]}>
+              <strong>{group.groupTitle}</strong>
+            </p>
+            <ul className={styles["lvl-card__list"]}>
+              {group.items.map((item, i) => (
+                <li key={i}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
       <button className={styles["lvl-card__cta"]}>{buttonText}</button>
     </div>
   );
